d8f2eb9c54ac37722be4a49c3cff0fe555abcb1e
Abhisek Dasgupta <33784411+LeonDasgupta@users.noreply.github.com>
2023-05-26 00:20:51 +0530

    Create gc_counter.py

---
+++ gc_counter.py
@@ -0,0 +1,10 @@
+rom Bio.Seq import Seq
+
+def gc_counter(dna):
+    x = dna.count("G") + dna.count("C")
+    y = x / len(dna)
+    count = y * 100
+    print(f'The GC percentage in your sequence is {count}')
+
+dna = Seq(input("Enter the DNA sequence without the identifiers:"))
+gc_counter(str(dna))
